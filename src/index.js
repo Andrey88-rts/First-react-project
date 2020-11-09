b3384147e997a6cc4b2439c7d9ed6dc901409b2b
Andrey <techpcb@mail.ru>
2020-11-09 23:00:03 +0400

    Рассмотрена работа с препроцессором JSX

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Header = () => {
+
+    return <h2>Hello world</h2>
+}
+
+const Field = () => {
+    const hold = "Enter";
+    const styled = {
+        width: '300px',
+        height: '20px',
+        color: 'red',
+        border: '2px solid green',
+        borderRadius: '5px'
+    }
+    return <input style={styled} type="text" placeholder={hold} autoComplete="" className="first" htmlFor="" />
+}
+
+const Btn = () => {
+    const text = "Log in";
+    const logged = true;
+    return <button>{logged ? "enter" : text}</button>
+}
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Field />
+            <Btn />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
